bbb380a908b60032775b98a93ec8159201dc305f
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2024-07-11 18:48:46 +0530

    Add files via upload

---
+++ BallController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horInput, 0.0f, verInput) * speed * Time.deltaTime;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement, ForceMode.Impulse);
+    }
+}
