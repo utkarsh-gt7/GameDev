628e6901b7d16717ebda4f0b4265d9afc23b1722
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2024-07-11 19:42:17 +0530

    Add files via upload

---
+++ Collectable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Player"){
+            Invoke("DestroyCollectables", 0.2f);
+        }
+    }
+    void DestroyCollectables(){
+        Destroy(this.gameObject);
+    }
+}
