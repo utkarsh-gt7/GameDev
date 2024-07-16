68c68a1b5cac5e55721f5a880d8e59dc8f29a8ee
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2024-07-16 18:49:59 +0530

    Create Conditional.cs

---
+++ Conditional.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If_Statements : MonoBehaviour
+{
+    // public int age;
+    public int score;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     if(age > 18){
+    //         Debug.Log("You are adult");// code to be executed if condition is true
+    //     }
+    //     else if(age < 18){
+    //         Debug.Log("You can't vote");// code to be executed if condition is false
+    //     }
+    //     else{
+    //         Debug.Log("you are 18");
+    //     }
+
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void CheckScore(int score)
+{
+    switch (score)
+    {
+        case 10:
+            Debug.Log("Perfect Score");
+            break;
+        case 5:
+            Debug.Log("The score is half");
+            break;
+        default:
+            Debug.Log("The score is not good enough");
+            break;
+    }
+}
+
+}
