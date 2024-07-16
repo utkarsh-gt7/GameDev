92be63362c7a3ef80e329bd8ced994e28e1d8613
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2024-07-16 18:53:00 +0530

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<11;i++){
+            Debug.Log("The value of i is " + i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
