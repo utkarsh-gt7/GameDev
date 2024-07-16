d86b4abae61d776e28f8483b9068b9035b28672f
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2024-07-16 18:44:20 +0530

    Add files via upload

---
+++ If_Statements.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If_Statements : MonoBehaviour
+{
+    // public int age;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if(age > 18){
+        //     Debug.Log("You are adult");// code to be executed if condition is true
+        // }
+        // else if(age < 18){
+        //     Debug.Log("You can't vote");// code to be executed if condition is false
+        // }
+        // else{
+        //     Debug.Log("you are 18");
+        // }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
